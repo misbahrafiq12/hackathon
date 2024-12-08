e4e0e00b6f4f78ed86cfafaf3dbfae82ba422b96
Jamila654 <136564835+Jamila654@users.noreply.github.com>
2024-12-08 16:10:06 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
               <CardDescription>Sedan</CardDescription>
             </CardHeader>
             <CardContent className="w-full flex flex-col items-center justify-center gap-4">
-              <Image src={"/car (2).png"} alt="" width={220} height={68} />
+              <Image src={"/car.png"} alt="" width={220} height={68} />
               <Image src={"/Spesification.png"} alt="" width={256} height={24} />
             </CardContent>
             <CardFooter className="w-full flex items-center justify-between">
